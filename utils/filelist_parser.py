7f6eab7b11f7b2b62d90e69cdeded3aa660378f6
MaT-PT <MaT-PT@users.noreply.github.com>
2024-03-28 12:44:09 +0100

    Add `$USERNAME` variable; split variable substitution functions to separate file

--- utils/filelist_parser.py
+++ utils/filelist_parser.py
@@ -4,14 +4,14 @@ import logging
 import subprocess
 import sys
 from dataclasses import dataclass, field
-from datetime import datetime
 from graphlib import TopologicalSorter
 from pathlib import Path, PurePath
-from typing import Any, Iterable, Iterator, Protocol, TypedDict, overload
+from typing import Any, Iterable, Iterator, TypedDict, overload
 
 import yaml
 
 from .config_parser import Config
+from .variable_utils import get_username, sub_vars
 
 if sys.version_info >= (3, 11):
     from typing import NotRequired, Self
@@ -19,17 +19,6 @@ else:
     from typing_extensions import NotRequired, Self
 
 LOGGER = logging.getLogger(__name__)
-
-
-class VarFunction(Protocol):
-    """Protocol for functions that can be called from a variable substitution in a string"""
-
-    def __call__(self, *args: str) -> str: ...
-
-
-VAR_FUNCTIONS: dict[str, VarFunction] = {
-    "PATH": lambda *args: str(Path(args[0])),
-}
 
 
 @dataclass
@@ -52,58 +41,6 @@ class MutableBool:
         if isinstance(other, MutableBool):
             return self.value == other.value
         return self.value == other
-
-
-def sub_vars_loop(s: str, var_dict: dict[str, str], upper: bool = True, max_iter: int = 10) -> str:
-    """Substitute variables in a string, repeatedly until no more substitutions are possible"""
-    for _ in range(max_iter):
-        new_s = s
-        for key, value in var_dict.items():
-            key = key.removeprefix("$")
-            new_s = new_s.replace(f"${key.upper() if upper else key}", value)
-        if new_s == s:
-            return new_s
-        s = new_s
-    LOGGER.warning(f"Max number of iterations reached while substituting variables in: {s}")
-    return s
-
-
-def sub_funcs(s: str, upper: bool = True) -> str:
-    """Run functions of the form of `${func_name:arg1,arg2,...}`"""
-    while True:
-        start = s.find("${")
-        if start == -1:
-            break
-        end = s.find("}", start)
-        if end == -1:
-            LOGGER.warning(f"Unterminated function call: {s[start:]}")
-            break
-        func = s[start + 2 : end]
-        LOGGER.debug(f"Found function: {func}")
-        if ":" not in func:
-            LOGGER.warning(f"Invalid function syntax: {func}")
-            break
-        func_name, args_str = func.split(":")
-        if func_name not in VAR_FUNCTIONS:
-            LOGGER.warning(f"Unknown function: {func_name}")
-        args = args_str.split(",")
-        if upper:
-            func_name = func_name.upper()
-        LOGGER.debug(f"Calling function: {func_name}({args})")
-        s = s[:start] + VAR_FUNCTIONS[func_name](*args) + s[end + 1 :]
-    return s
-
-
-def sub_vars(s: str, var_dict: dict[str, str], upper: bool = True, max_iter: int = 10) -> str:
-    if "$" not in s:
-        return s
-    if "TIME" not in var_dict:
-        var_dict["TIME"] = datetime.now().strftime("%H.%M.%S")
-    if "DATE" not in var_dict:
-        var_dict["DATE"] = datetime.now().strftime("%Y-%m-%d")
-    res = sub_vars_loop(s, var_dict, upper, max_iter)
-    res = sub_funcs(res)
-    return res
 
 
 @dataclass(frozen=True)
@@ -261,6 +198,8 @@ class FileList:
                     "ENTRYPATH": str(entry_path),
                     "FILENAME": entry_path.name,
                 }
+                if (username := get_username(entry_path)) is not None:
+                    extra_vars["USERNAME"] = username
             if out_dir is None:
                 out_dir = Path(".")
             elif isinstance(out_dir, str):
@@ -269,6 +208,7 @@ class FileList:
                 file_path = out_dir / self.file.path
             elif isinstance(file_path, str):
                 file_path = Path(file_path)
+
             cmd = self.get_command(file_path, out_dir, extra_vars, extra_args)
             if cmd is None:
                 return None
---
+++ utils/variable_utils.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+import logging
+from datetime import datetime
+from pathlib import Path, PurePath
+from typing import Protocol
+
+LOGGER = logging.getLogger(__name__)
+
+
+class VarFunction(Protocol):
+    """Protocol for functions that can be called from a variable substitution in a string"""
+
+    def __call__(self, *args: str) -> str: ...
+
+
+VAR_FUNCTIONS: dict[str, VarFunction] = {
+    "PATH": lambda *args: str(Path(args[0])),
+}
+
+
+def sub_vars_loop(s: str, var_dict: dict[str, str], upper: bool = True, max_iter: int = 10) -> str:
+    """Substitute variables in a string, repeatedly until no more substitutions are possible"""
+    for _ in range(max_iter):
+        new_s = s
+        for key, value in var_dict.items():
+            key = key.removeprefix("$")
+            new_s = new_s.replace(f"${key.upper() if upper else key}", value)
+        if new_s == s:
+            return new_s
+        s = new_s
+    LOGGER.warning(f"Max number of iterations reached while substituting variables in: {s}")
+    return s
+
+
+def sub_funcs(s: str, upper: bool = True) -> str:
+    """Run functions of the form of `${func_name:arg1,arg2,...}`"""
+    while True:
+        start = s.find("${")
+        if start == -1:
+            break
+        end = s.find("}", start)
+        if end == -1:
+            LOGGER.warning(f"Unterminated function call: {s[start:]}")
+            break
+        func = s[start + 2 : end]
+        LOGGER.debug(f"Found function: {func}")
+        if ":" not in func:
+            LOGGER.warning(f"Invalid function syntax: {func}")
+            break
+        func_name, args_str = func.split(":")
+        if func_name not in VAR_FUNCTIONS:
+            LOGGER.warning(f"Unknown function: {func_name}")
+        args = args_str.split(",")
+        if upper:
+            func_name = func_name.upper()
+        LOGGER.debug(f"Calling function: {func_name}({args})")
+        s = s[:start] + VAR_FUNCTIONS[func_name](*args) + s[end + 1 :]
+    return s
+
+
+def sub_vars(s: str, var_dict: dict[str, str], upper: bool = True, max_iter: int = 10) -> str:
+    if "$" not in s:
+        return s
+    if "TIME" not in var_dict:
+        var_dict["TIME"] = datetime.now().strftime("%H.%M.%S")
+    if "DATE" not in var_dict:
+        var_dict["DATE"] = datetime.now().strftime("%Y-%m-%d")
+    res = sub_vars_loop(s, var_dict, upper, max_iter)
+    res = sub_funcs(res)
+    return res
+
+
+def get_username(path: str | PurePath) -> str | None:
+    """Get the username from a path, if applicable"""
+    if isinstance(path, str):
+        path = PurePath(path)
+    if (path.is_relative_to("Users") or path.is_relative_to("home")) and len(path.parts) > 1:
+        return path.parts[1]
+    if path.is_relative_to("root"):
+        return "root"
+    return None
